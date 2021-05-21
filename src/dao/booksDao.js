4c765d95f0ceb7722de0542657b5aed1c86d616f
Fatih <fatihermetin@gmail.com>
2021-05-21 17:14:37 +0300

    Implemented books dao for db

---
+++ src/dao/booksDao.js
@@ -0,0 +1,20 @@
+
+let dapp
+let books
+
+class BooksDao {
+    static async injectDb(connection){
+        if(dapp) return
+
+        try {
+            dapp = connection.db(process.env.DB_NAME)
+            books = connection.db(process.env.DB_NAME).collection(BOOKS_COLLECTION_NAME)
+        } catch (error) {
+            console.error(
+                `Unable to establish a collection handle in moviesDAO: ${error}`,
+              )
+        }
+    }
+}
+
+module.exports =  BooksDao
