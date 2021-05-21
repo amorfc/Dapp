507ee1d8b6697547fa2467158c69c30c063a446c
Fatih <fatihermetin@gmail.com>
2021-05-21 19:30:27 +0300

    Added BodyParser For Requests

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const app = express()
 
 process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use('/api/v1/user', users)
 app.use('*', (req, res) => res.status(404).json({ error: "not found anything amerovski urlde problemo" }))
 
