06b4aec30a3e3133db4067ca0c08c2e8085826e0
Fatih <fatihermetin@gmail.com>
2021-05-21 18:19:01 +0300

    Fix Typo

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const users = require('./api/users/users.route')
 
 const app = express()
 
 process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 
+app.use('/api/v1/user', users)
 app.use('*', (req, res) => res.status(404).json({ error: "not found anything amerovski urlde problemo" }))
 
 module.exports = app
