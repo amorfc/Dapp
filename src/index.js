8f357c3869adaf52436c0245b642f5b46fece803
Fatih <fatihermetin@gmail.com>
2021-05-21 16:59:57 +0300

    Implemented db connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 const server = require('./server.js')
+const { MongoClient } = require('mongodb')
+const UsersDao = require('./dao/usersDao')
 
 const port = process.env.PORT
 
-server.listen(port,()=>{
-    console.log(`listening on port ${port}`);
-})
+MongoClient.connect(
+    process.env.MONGO_DB_DAPP_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        poolSize: 50,
+    })
+    .catch((error) => {
+        console.log(`Error Occured When MongoDb Try Connect ${error}`)
+        process.exit(1)
+    })
+    .then(async (mongoClient) => {
+        //Do injections
+        await UsersDao.injectDb(mongoClient)
+        server.listen(port, () => {
+            console.log(`Listenint on port ${port}`);
+        })
+    })
+
