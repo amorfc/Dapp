0634d98a6fe4c4dabade3f8d0e2d5ed3f54a3b5c
Fatih <fatihermetin@gmail.com>
2021-05-22 01:50:56 +0300

    Deleted deprecated body-parser module

--- src/api/users/users.controller.js
+++ src/api/users/users.controller.js
@@ -2,7 +2,13 @@ class UserController {
     static async register(req, res){
 
         const userFromBody = req.body
-        
+        //Validations must 
+        //Add user to DB
+        //Res send properly with filtered from cases
+            //If user exixsts
+            //If password wrong
+            //If everything good
+            //If unknown error occured
         console.log(req.body);
         res.json({
             "done":"done"
--- src/server.js
+++ src/server.js
@@ -7,8 +7,7 @@ const app = express()
 
 process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/api/v1/user', users)
 app.use('*', (req, res) => res.status(404).json({ error: "not found anything amerovski urlde problemo" }))
