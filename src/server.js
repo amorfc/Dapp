8b1f91f6f82671413831ea66e35f15b915aa3d4a
Fatih <fatihermetin@gmail.com>
2021-05-24 01:36:11 +0300

    Added card route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const users = require('./api/users/users.route')
+const cards = require('./api/cards/cards.route')
 
 const app = express()
 
@@ -10,6 +11,7 @@ process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 app.use(express.json())
 
 app.use('/api/v1/user', users)
+app.use('/api/v1/card', cards)
 app.use('*', (req, res) => res.status(404).json({ error: "not found anything amerovski urlde problemo" }))
 
 module.exports = app
