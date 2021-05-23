08317f6f1d6df3a08d61399524f6b9a3e5e399fb
Fatih <fatihermetin@gmail.com>
2021-05-24 01:36:28 +0300

    Implemented Card Dao

--- src/dao/cardsDao.js
+++ src/dao/cardsDao.js
@@ -1,18 +1,24 @@
-
-
 let dapp
 let cards
 
 class CardsDao {
-    static async injectDb(connection){
-        if(dapp) return
+    static async injectDb(connection) {
+        if (dapp) return
 
         try {
-            dapp =  connection.db(process.env.DB_NAME)
-            cards =  connection.db(process.env.DB_NAME).collection(process.env.CARDS_COLLECTION_NAME)
+            dapp = connection.db(process.env.DB_NAME)
+            cards = connection.db(process.env.DB_NAME).collection(process.env.CARDS_COLLECTION_NAME)
         } catch (error) {
             console.error(
                 `Unable to establish a collection handle in moviesDAO: ${error}`)
+        }
+    }
+    static async addCard(card) {
+        try {
+            await cards.insertOne(card)
+            return {success: true}
+        } catch (error) {
+            return {error: error}
         }
     }
 }
