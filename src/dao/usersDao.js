ea587f6107f12a59b6a09373765ed33c32b7ec5c
Fatih <fatihermetin@gmail.com>
2021-05-23 18:08:16 +0300

    Added database user insertion

--- src/dao/usersDao.js
+++ src/dao/usersDao.js
@@ -5,16 +5,49 @@ let users
 
 class UsersDao {
     static async injectDb(connection) {
-        if (dapp)return
-        
+        if (dapp) return
+
         try {
             dapp = await connection.db(process.env.MONGO_DB_DAPP_NAME)
             users = await connection.db(process.env.DB_NAME).collection(process.env.USERS_COLLECTION_NAME)
         } catch (error) {
             console.error(
                 `Unable to establish a collection handle in moviesDAO: ${error}`,
-              )
+            )
         }
+    }
+    static async addUser(newUserInfo) {
+        try {
+            const newUserDoc = {
+                schoolNo: newUserInfo.schoolNo,
+                password: newUserInfo.password,
+                preferences: newUserInfo.preferences
+            }
+            await users.insertOne(newUserDoc)
+            return { success: true }
+        } catch (error) {
+            if (error.code === 11000) {
+                return {
+                    error: "User Already Exists!!!"
+                }
+            }
+            return { error: e }
+        }
+    }
+    static async getUserWithSchoolNo(schoolNo) {
+        const result = {
+            data: null,
+            success: null,
+            error: null
+        }
+        try {
+            const getUserResult = await users.findOne({ schoolNo: schoolNo })
+            result.data = getUserResult
+            result.success = true
+        } catch (error) {
+            result.error = error
+        }
+        return result
     }
 }
 
