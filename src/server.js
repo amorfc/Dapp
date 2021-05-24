dfd956edf6020292c95c307c549eec62ab3f3ec5
Fatih <fatihermetin@gmail.com>
2021-05-24 18:01:18 +0300

    Added book endpoint

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const users = require('./api/users/users.route')
 const cards = require('./api/cards/cards.route')
+const books = require('./api/books/books.route')
 
 const app = express()
 
@@ -12,7 +13,8 @@ app.use(express.json())
 
 app.use('/api/v1/user', users)
 app.use('/api/v1/card', cards)
-app.use('*', (req, res) => res.status(404).json({ error: "not found anything amerovski urlde problemo" }))
+app.use('/api/v1/book', books)
+app.use('*', (req, res) => res.status(404).json({ error: "not found anything amerovski URLde problemo" }))
 
 module.exports = app
 
