1fff0c69bb4aa6e83561bbc4c93cfc6490356469
Fatih <fatihermetin@gmail.com>
2021-06-19 23:08:04 +0300

    login request changed post method

--- src/api/users/users.route.js
+++ src/api/users/users.route.js
@@ -4,7 +4,7 @@ const UserController = require('./users.controller')
 const router = new Router()
 
 router.route('/register').post(UserController.register)
-router.route('/login').get(UserController.login)
+router.route('/login').post(UserController.login)
 router.route('/logout').post(UserController.logout)
 
 
